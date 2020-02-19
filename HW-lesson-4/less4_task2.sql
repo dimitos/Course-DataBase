9a2d3ba8dbe320097a57a58f7bfc2d3566a93f5a
dimitos <kurort26@mail.ru>
2020-02-19 18:04:39 +0300

    домашка 4 изменения по замечаниям препода

--- HW-lesson-4/less4_task2.sql
+++ HW-lesson-4/less4_task2.sql
@@ -6,6 +6,9 @@
 
 USE vk;
 
-SELECT firstname FROM users GROUP BY firstname ORDER BY firstname ASC;
+SELECT id, firstname FROM users GROUP BY firstname ORDER BY firstname ASC;
 -- выбрать firstname из таблицы vk.users  без повторений в колонке firstname отсортировать firstname по возрастанию.
 -- из 100 users выбирает уникальных 93 и сортирует по возрастанию.
+
+# отсортировать можно с мпомощью DISTINCT
+SELECT  DISTINCT firstname FROM users;
--- HW-lesson-4/less4_task3.sql
+++ HW-lesson-4/less4_task3.sql
@@ -4,19 +4,38 @@
  Предварительно добавить такое поле в таблицу profiles со значением по умолчанию = true (или 1)
 */
 
-
 use vk;
+
+/*
+# ПЕРВЫЙ ВАРИАНТ
+# добавим поле is_active в таблицу profiles со значением по умолчанию = true
+ALTER TABLE `vk`.`profiles` 
+ADD COLUMN `is_active` SET('true', 'false') NOT NULL DEFAULT 'false' AFTER `created_at`,
+ADD INDEX `is_active` (`is_active` ASC) VISIBLE;
+
+# заменим false на true в тех строчках, где возраст больше 18
+UPDATE `vk`.`profiles` 
+SET `is_active` = 'true' 
+WHERE `is_active` = 'false' 
+AND (YEAR(CURRENT_DATE)-YEAR(`birthday`))-(RIGHT(CURRENT_DATE,5)<RIGHT(`birthday`,5)) >= 18;
+
+# изменил 60 строк открыли доступ 60 пользователям
+SELECT *  FROM `vk`.`profiles`;
+*/
+
+# ВТОРОЙ ВАРИАНТ  используем тип данных BIT
 
 -- добавим поле is_active в таблицу profiles со значением по умолчанию = true
 ALTER TABLE `vk`.`profiles` 
-ADD COLUMN `is_active` SET('true', 'false') NOT NULL DEFAULT 'true' AFTER `created_at`,
+ADD COLUMN `is_active` BIT NOT NULL AFTER `created_at`,  -- при NOT NULL по умолчанию присваивается 0
 ADD INDEX `is_active` (`is_active` ASC) VISIBLE;
 
--- заменим true на false в тех строчках, где возраст меньше 18
+-- заменим 0 на 1 в тех строчках, где возраст больше 18
 UPDATE `vk`.`profiles` 
-SET `is_active` = 'false' 
-WHERE `is_active` = 'true' 
-AND (YEAR(CURRENT_DATE)-YEAR(`birthday`))-(RIGHT(CURRENT_DATE,5)<RIGHT(`birthday`,5)) < 18;
+SET `is_active` = 1       -- открываем доступ
+WHERE `is_active` = 0 
+AND (YEAR(CURRENT_DATE)-YEAR(`birthday`))-(RIGHT(CURRENT_DATE,5)<RIGHT(`birthday`,5)) >= 18;
 
--- изменил 40 строк
+-- зменил 60 строк открыли доступ 60 пользователям
 SELECT *  FROM `vk`.`profiles`;
+
